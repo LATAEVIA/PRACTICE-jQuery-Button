b18301058546da18bb6841a9b08407d86bc402b6
Anthony Gold and LaTaevia Berry <anthony@>
2016-03-22 12:01:29 -0700

    add border, margin top, and new button for border

--- js/scripts.js
+++ js/scripts.js
@@ -13,4 +13,12 @@ $(document).ready(function() {
     $("body").removeClass();
     $("body").addClass("red-background");
   });
+  $("button#white").click(function() {
+    $("body").removeClass();
+    $("body").addClass("revert-colors");
+  });
+  $("button#blue").click(function() {
+    // $("body").removeClass();
+    $("#blue-text-border").addClass("add-border");
+  });
 });
